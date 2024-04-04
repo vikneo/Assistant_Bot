cbd20d021c2eeaaa3f8c0b74ecd0415d23d9d2ac
Viktor Martynov <vm-viktor@mail.ru>
2024-04-04 13:01:07 +0700

    created config.py file

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+import telebot
+
+load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+mess_to_start = ('Добро пожаловать! <b>{0.first_name}</b>.\n'
+                 'Я карманный справочник!\n'
+                 'Для просмотра команд нажмите <b>/help</b>')
+
+mess_to_help = '<b>Краткая помощь по командам:</b>\n' \
+               ' - <b>/low_price</b> - поиск отелей в городах по минимальным ценам\n' \
+               ' - <b>/high_price</b> - поиск отелей в городах по максимальным ценам\n' \
+               ' - <b>/best_deal</b> - поиск отелей в городах по диапазону цен\n' \
+               ' - <b>/history</b> - история просмотренных результатов'
